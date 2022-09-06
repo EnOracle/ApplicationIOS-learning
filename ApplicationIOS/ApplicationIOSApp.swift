7f48a62222fb25d72fb6503411b1ae3f319ff108
sara <c.project.sara@gmail.com>
2022-09-06 10:53:42 -0400

    Initial Commit

---
+++ ApplicationIOS/ApplicationIOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  ApplicationIOSApp.swift
+//  ApplicationIOS
+//
+//  Created by Sara on 2022-09-06.
+//
+
+import SwiftUI
+
+@main
+struct ApplicationIOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
